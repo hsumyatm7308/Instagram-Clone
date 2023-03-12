9085fa569d6ff9f552387a3aacca4ae81c6b53c4
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-12 18:29:22 +0630

    6:29

--- folder/app.js
+++ folder/app.js
@@ -4,4 +4,50 @@ const getmorebtns = document.getElementById('morebtns');
 
 getmorebtns.addEventListener("click",function(){
    getmoresettings.classList.toggle('moretoggles');
+});
+
+
+
+const getpostimg = document.querySelectorAll('.socialpost')
+const rightarrow = document.querySelector('.fa-circle-chevron-right');
+const leftarrow = document.querySelector('.fa-circle-chevron-left');
+
+let curimg = 0;
+
+leftarrow.addEventListener("click",function(){
+  
 })
+
+rightarrow.addEventListener("click",function(){
+
+   curimg++
+   if(curimg > getpostimg.length-1){
+      curimg = getpostimg.length-1;
+   }
+   socialpostright();
+});
+
+
+function socialpostright() {
+   for(var x = 0; x< getpostimg.length; x++){
+    if(x === curimg){
+      getpostimg[x].classList.add('postaespa');
+      getpostimg[curimg-1].classList.replace('translate-x-full','-translate-x-full');
+      getpostimg[curimg-1].style.transition = "all 1s"
+    }else{
+      getpostimg[x].classList.remove('postaespa');
+
+    }
+   }
+}
+
+
+function socialpostleft(){
+   for(var x = 0; x < getpostimg.length; x++){
+      if(x === curimg){
+         
+      }
+   }
+}
+
+
