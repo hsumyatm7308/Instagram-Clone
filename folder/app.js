8b4add8f4479b3a2e215617f637ad00afe810110
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-12 09:41:46 +0630

    9:41

---
+++ folder/app.js
@@ -0,0 +1,6 @@
+const getmoresettings = document.getElementById('moresettings');
+const getmorebtns = document.getElementById('morebtns');
+
+getmorebtns.addEventListener("click",function(){
+   getmoresettings.classList.toggle('moretoggles');
+})
