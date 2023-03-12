41a34d85e6cd0ca20612e84db030a196df070e38
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-03-12 22:31:29 +0630

    10:30PM

--- folder/app.js
+++ folder/app.js
@@ -12,24 +12,31 @@ const getpostimg = document.querySelectorAll('.socialpost')
 const rightarrow = document.querySelector('.fa-circle-chevron-right');
 const leftarrow = document.querySelector('.fa-circle-chevron-left');
 
+const getdots = document.querySelectorAll('.dots')
+
 let curimg = 0;
+let actcolor = 0;
 
 leftarrow.addEventListener("click",function(){
   curimg--;
+  actcolor--;
   if(curimg < 0){
    curimg = 0;
   }
   socialpostleft();
+  dotsfun();
 })
 
 rightarrow.addEventListener("click",function(){
 
    curimg++;
+   actcolor++;
 
    if(curimg > getpostimg.length-1){
       curimg = getpostimg.length-1;
    }
    socialpostright();
+   dotsfun();
 });
 
 
@@ -38,7 +45,7 @@ function socialpostright() {
     if(x === curimg){
       getpostimg[x].classList.add('postaespa');
       getpostimg[curimg-1].classList.replace('left-full','-left-full');
-      getpostimg[curimg-1].style.transition = "all 1s"
+      getpostimg[curimg-1].style.transition = "all 1s";
     }else{
       getpostimg[x].classList.remove('postaespa');
 
@@ -47,6 +54,13 @@ function socialpostright() {
     if(curimg >= 1){
       leftarrow.style.display = 'block';
     }
+
+    if(curimg >= 4){
+      rightarrow.style.display = "none"
+    }
+
+
+   
    }
 }
 
@@ -68,7 +82,36 @@ function socialpostleft(){
       if(curimg < 1){
          leftarrow.style.display = "none";
       }
+
+      if(curimg < 4){
+         rightarrow.style.display = "block"
+      }
+    
+
+
    }
+}
+
+
+function dotsfun(){
+   for(var x = 0; x < getdots.length; x++){
+      if(x === actcolor){
+         getdots[x].classList.add('active-color');
+      }else{
+         getdots[x].classList.remove('active-color');
+      }
+   }
+
+}
+
+
+const heart = document.querySelector('.heartnoti')
+
+for(var x = 0; x < getpostimg.length; x++){
+   getpostimg[x].addEventListener("dblclick",function(){
+      // heart.style. = "red";
+      heart.innerHTML = `<i class="fa-solid fa-heart text-red-500 mr-2"></i>`
+   });
 }
 
 
